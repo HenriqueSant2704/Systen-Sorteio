34f79cdda3cb0ebd9e6acd6bc48dbe6425d92f9d
Miguel Santana <henriquesantanamiguel@gmail.com>
2026-03-28 23:42:50 -0300

    mascaras

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,5 +1,85 @@
 /*===========================================================================
 
+    Mascaras de Formatação
+
+============================================================================*/
+
+// ================= CPF =================
+const cpfInput = document.getElementById("cpfInput");
+
+function mascaraCPF(valor) {
+    valor = valor.replace(/\D/g, "");
+    valor = valor.slice(0, 11);
+
+    valor = valor.replace(/(\d{3})(\d)/, "$1.$2");
+    valor = valor.replace(/(\d{3})(\d)/, "$1.$2");
+    valor = valor.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+
+    return valor;
+}
+
+cpfInput.addEventListener("input", () => {
+    cpfInput.value = mascaraCPF(cpfInput.value);
+});
+
+// ================= TELEFONE =================
+const telefoneInput = document.getElementById("telefoneInput");
+
+function mascaraTelefone(valor) {
+    valor = valor.replace(/\D/g, "");
+    valor = valor.slice(0, 11);
+
+    valor = valor.replace(/^(\d{2})(\d)/g, "($1) $2");
+    valor = valor.replace(/(\d{5})(\d)/, "$1-$2");
+
+    return valor;
+}
+
+telefoneInput.addEventListener("input", () => {
+    telefoneInput.value = mascaraTelefone(telefoneInput.value);
+});
+
+// ================= Nome e Cidade =================
+const nomeInput = document.getElementById("nomeInput");
+const cidadeInput = document.getElementById("cidadeInput");
+
+function apenasLetras(valor) {
+    return valor.replace(/[^a-zA-ZÀ-ÿ\s]/g, "");
+
+}
+
+nomeInput.addEventListener("input", () => {
+    nomeInput.value = apenasLetras(nomeInput.value);
+});
+
+cidadeInput.addEventListener("input", () => {
+    cidadeInput.value = apenasLetras(cidadeInput.value);
+});
+
+
+function aplicarMascaraPix(tipo, valor) {
+
+    if (tipo === "cpf") {
+        valor = valor.replace(/\D/g, "");
+        return mascaraCPF(valor);
+    }
+
+    if (tipo === "telefone") {
+        valor = valor.replace(/\D/g, "");
+        return mascaraTelefone(valor);
+    }
+
+    return valor;
+}
+
+function emailValido(email) {
+    const temArroba = email.includes("@");
+    const temPonto = email.split("@")[1]?.includes(".");
+    return temArroba && temPonto;
+}
+
+/*===========================================================================
+
     Barra de Progresso
 
 ============================================================================*/
@@ -13,20 +93,23 @@ const botao = document.getElementById("proximoButton");
 const botaoVoltar = document.getElementById("voltarButton");
 const textoBotao = document.getElementById("textoBotao");
 const iconeBotao = document.getElementById("iconeBotao");
-
+const erroEmail = document.getElementById("erroEmail");
+const erroPix = document.getElementById("erroPix");
 
 botaoVoltar.style.display = "none";
 
 botao.addEventListener("click", (e) => {
     e.preventDefault();
 
-    if (etapaAtual < etapasForm.length - 1) {
+    if (!validarEtapaAtual()) {
+        return;
+    }
 
+    if (etapaAtual < etapasForm.length - 1) {
 
         etapasForm[etapaAtual].classList.remove("ativa");
         etapaAtual++;
         etapasForm[etapaAtual].classList.add("ativa");
-
 
         etapasTopo.forEach((etapa, index) => {
             if (index <= etapaAtual) {
@@ -34,13 +117,10 @@ botao.addEventListener("click", (e) => {
             }
         });
 
-
         let porcentagem = (etapaAtual / (etapasForm.length - 1)) * 100;
         linha.style.width = porcentagem + "%";
 
-
         botaoVoltar.style.display = etapaAtual === 0 ? "none" : "block";
-
 
         if (etapaAtual === etapasForm.length - 1) {
             textoBotao.textContent = "Finalizar Cadastro";
@@ -49,7 +129,8 @@ botao.addEventListener("click", (e) => {
         }
 
     } else {
-        alert("Cadastro finalizado 🚀");
+        document.querySelector("form").style.display = "none";
+        document.getElementById("telaSucesso").style.display = "block";
     }
 });
 
@@ -57,11 +138,9 @@ botaoVoltar.addEventListener("click", () => {
 
     if (etapaAtual > 0) {
 
-
         etapasForm[etapaAtual].classList.remove("ativa");
         etapaAtual--;
         etapasForm[etapaAtual].classList.add("ativa");
-
 
         etapasTopo.forEach((etapa, index) => {
             if (index <= etapaAtual) {
@@ -71,13 +150,10 @@ botaoVoltar.addEventListener("click", () => {
             }
         });
 
-
         let porcentagem = (etapaAtual / (etapasForm.length - 1)) * 100;
         linha.style.width = porcentagem + "%";
 
-
         botaoVoltar.style.display = etapaAtual === 0 ? "none" : "block";
-
 
         textoBotao.textContent = "Continuar";
         botao.classList.remove("finalizar");
@@ -85,6 +161,89 @@ botaoVoltar.addEventListener("click", () => {
     }
 });
 
+function validarEtapaAtual() {
+    const inputs = etapasForm[etapaAtual].querySelectorAll("input");
+
+    for (let input of inputs) {
+
+        if (input.value.trim() === "") {
+            input.style.borderColor = "red";
+            input.focus();
+            return false;
+        }
+
+        if (input.type === "email") {
+            if (!emailValido(input.value)) {
+                input.style.borderColor = "red";
+
+                erroEmail.textContent = "Digite um email válido (ex: nome@email.com)";
+                erroEmail.style.display = "block";
+
+                input.focus();
+                return false;
+            } else {
+                erroEmail.style.display = "none";
+            }
+        }
+
+
+        if (input.id === "cpfInput" && input.value.length < 14) {
+            input.style.borderColor = "red";
+            input.focus();
+            return false;
+        }
+
+
+        if (input.id === "telefoneInput" && input.value.length < 15) {
+            input.style.borderColor = "red";
+            input.focus();
+            return false;
+        }
+
+        if (input.id === "inputPix") {
+
+            if (tipoPix === "email") {
+                if (!emailValido(input.value)) {
+                    input.style.borderColor = "red";
+
+                    erroPix.textContent = "Digite um email válido para o PIX";
+                    erroPix.style.display = "block";
+
+                    input.focus();
+                    return false;
+                } else {
+                    erroPix.style.display = "none";
+                }
+            }
+
+            if (tipoPix === "cpf" && input.value.length < 14) {
+                input.style.borderColor = "red";
+
+                erroPix.textContent = "CPF incompleto";
+                erroPix.style.display = "block";
+
+                input.focus();
+                return false;
+            }
+
+            if (tipoPix === "telefone" && input.value.length < 15) {
+                input.style.borderColor = "red";
+
+                erroPix.textContent = "Telefone incompleto";
+                erroPix.style.display = "block";
+
+                input.focus();
+                return false;
+            }
+
+            erroPix.style.display = "none";
+        }
+
+        input.style.borderColor = "";
+    }
+
+    return true;
+}
 
 /* =================================================================
 
@@ -95,38 +254,42 @@ botaoVoltar.addEventListener("click", () => {
 const opcoes = document.querySelectorAll(".opcao");
 const inputPix = document.getElementById("inputPix");
 
+let tipoPix = "email";
+
 opcoes.forEach(opcao => {
     opcao.addEventListener("click", () => {
 
-
         opcoes.forEach(o => o.classList.remove("ativa"));
-
 
         opcao.classList.add("ativa");
 
-        const tipo = opcao.dataset.tipo;
+        tipoPix = opcao.dataset.tipo;
 
+        inputPix.value = "";
+        erroPix.style.display = "none";
 
-        if (tipo === "email") {
+        if (tipoPix === "email") {
             inputPix.placeholder = "seu@email.com";
         }
 
-        if (tipo === "cpf") {
+        if (tipoPix === "cpf") {
             inputPix.placeholder = "xxx.xxx.xxx-xx";
         }
 
-        if (tipo === "telefone") {
+        if (tipoPix === "telefone") {
             inputPix.placeholder = "(00) 00000-0000";
         }
 
-        if (tipo === "aleatoria") {
+        if (tipoPix === "aleatoria") {
             inputPix.placeholder = "Chave aleatória";
         }
     });
 });
 
+inputPix.addEventListener("input", () => {
+    inputPix.value = aplicarMascaraPix(tipoPix, inputPix.value);
+});
+
 /* =================================================================
-
-
 
 ====================================================================*/
